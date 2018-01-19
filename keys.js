d1a44a5cc646006f73c2398a4fdbb983f4d5be62
Mike Morales <mikemorale@live.com>
2018-01-19 02:04:52 -0500

    keys

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 exports.twitterkeys = {
 	
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secre '',
+	consumer_key: 'aKWjBYm2UoAjJ5rNDH27kzQsM ',
+	consumer_secret: 'stZR8Szdh9Ziym6hglmR1V0EHWItY8sb0IOzocWTegQfe32Jaf',
+	access_token_key: '954227543975972864-we4LZygSsW49nTkwJMIhEdjXjJZia1t',
+	access_token_secre 'zoWcYTB96ZSZf0CutkszpgbflINYiFinYG9BdbytVGgTs',
 }
 
 exports.spotify = {
