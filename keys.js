526eb803ca3926624cddf80a34a128690892b031
Mike Morales <mikemorale@live.com>
2018-01-18 23:28:07 -0500

    no keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.twitterkeys = {
+	
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secre '',
+}
+
+exports.spotify = {
+	id:  process.env.SPOTIFY.ID,
+	secret : process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,6 @@
+//  Data from keys.js
+
+var keys = require ('./keys.js');
+var fs = require ("fs");
+var request = require ('request');
+var spotify = require ('spotify');
